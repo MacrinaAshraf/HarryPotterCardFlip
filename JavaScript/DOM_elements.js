2266a5825f61a0aa428488d80288923d9cb4fa50
MacrinaAshraf <macrina1997@gmail.com>
2019-11-30 14:39:22 +0200

    added DOM_elements.js to get clickables from HTML

---
+++ JavaScript/DOM_elements.js
@@ -0,0 +1,24 @@
+var clickables = {
+	page1 : {
+		howToPlayBtn : document.getElementById('howtoplay');
+		aboutUsBtn : document.getElementById('aboutus');
+		charImg : document.getElementsByClassName('photo');
+		charHarry : document.getElementById('harry');
+		charRon : document.getElementById('ron');
+		charHermione : document.getElementById('hermione');
+		easyLevel : document.getElementById('easy');
+		mediumLevel : document.getElementById('medium');
+		hardLevel : document.getElementById('hard');
+		playBtn : document.getElementById('play');
+	}
+
+	page2 : {
+		cells : document.getElementsByClassName('cellDiv');
+		menuBtn : document.getElementById('menuBtn');
+	}
+};
+
+var pages = {
+	page1 : document.getElementById('page1');
+	page2 : document.getElementById('page2');
+};
