96875a3b03c4119a8e459ff17830aa11fc33805b
Rehab Ayman <rehabayman.fcis@gmail.com>
2019-12-05 17:12:08 +0200

    Update Character.js
    Co-Authored-By: macrinaashraf <macrinaashraf@users.noreply.github.com>
    Co-Authored-By: nahlaahmed97 <nahlaahmed97@users.noreply.github.com>

--- JavaScript/character.js
+++ JavaScript/character.js
@@ -1,10 +1,12 @@
 
-var character = function(charName)
+var character = function()
 {
-    this.charName = charName;
+    this.charName;
+    //these arrays should store the sources of the characters' photos
     this.harry = ['harry'];
     this.hermonie = ['hermonie'];
     this.ron = ['ron'];
+    //this array will be returned
     this.charArr;
 }
 
@@ -12,7 +14,7 @@ character.prototype.setCharArr = function(chosenChar)
 {
     if(chosenChar === "harry")
         this.charArr = this.harry;
-    else if(chosenChar === "hermonie")
+    else if(chosenChar === "hermione")
         this.charArr = this.hermonie;
     else
         this.charArr = this.ron;
@@ -23,27 +25,41 @@ character.prototype.getCharArr = function()
     return this.charArr;
 }
 
-function prepareCharacter()
-{
-    this.classList.toggle('clicked');
+var obj = new character();
+var divClicked;
+function addEve() {
+    var divs = document.getElementsByClassName('photo');
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].addEventListener('click', prepareCharacter);
+    }
+    
 }
 
-var divs = document.getElementsByClassName('photo');
-for(var i = 0; i < divs.length; i++)
-{
-    divs[i].addEventListener('click', prepareCharacter);
-}
 
-// var playButton = document.getElementsByClassName('play')[0];
+function prepareCharacter() {
+    // this.classList.add('clicked');
+    if (divClicked != this) {
+        
+        divClicked = this;
+        obj.setCharArr(divClicked.id);
+        // this.classList.remove('clicked');
+    }
+    console.log(obj);
+    //return divClicked;
+}
+console.log(divClicked);
+
+
 
 // function chooseChar()
 // {
-//     for(var i = 0; i < 3; i++)
-//     {
-//         if(divs[i].classList.contains('clicked'))
-//             return divs[i];
-//     }
+//     return divClicked.id;
 // }
 
-// console.log(chooseChar().id);
-// var obj = new character(chooseChar().id);
+addEve();
+
+// obj.getCharArr();
+// console.log(obj);
+
+//console.log(obj);
+//var playButton = document.getElementsByClassName('play')[0];
