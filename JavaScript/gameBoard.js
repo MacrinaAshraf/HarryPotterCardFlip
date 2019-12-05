1582646ebae887bb3a1472fb6398bb7288a2eb44
Rehab Ayman <rehabayman.fcis@gmail.com>
2019-12-05 14:42:26 +0200

    Finished OOP for gameBoard.js

--- JavaScript/gameBoard.js
+++ JavaScript/gameBoard.js
@@ -1,7 +1,6 @@
-//This Code will be Modified for OOP but this only kept now for testing the card rotation
-var gameBoard = function(cards)
+
+var gameBoard = function()
 {
-    this.cards = cards;
     this.firstCard = null;
     this.secondCard = null;
     this.hasFlippedCard = false;
@@ -9,30 +8,46 @@ var gameBoard = function(cards)
     
 }
 
-gameBoard.prototype.flipCard = function()
+gameBoard.prototype.checkMatch = function(firstCard, secondCard)
 {
-    this.classList.add('flip');
+    if(firstCard.children[0].src === secondCard.children[0].src)
+        return true;
+    return false;
+}
 
+gameBoard.prototype.resetBoardVars = function()
+{
+    this.firstCard = null;
+    this.secondCard = null;
+    this.hasFlippedCard = false;
+    this.lockBoard = false;
+}
+
+var gameBoardObj = new gameBoard();
+flipCard = function()
+{
     if(gameBoardObj.lockBoard)
         return;
     
     if(this === gameBoardObj.firstCard)
         return;
 
+    this.classList.add('flip');
+    
     if(gameBoardObj.hasFlippedCard)
     {
         gameBoardObj.secondCard = this;
         if(gameBoardObj.checkMatch(gameBoardObj.firstCard, gameBoardObj.secondCard))
         {
             //disable both cards from being flipped back again
-            gameBoardObj.firstCard.removeEventListener('click', gameBoardObj.flipCard);
-            gameBoardObj.secondCard.removeEventListener('click', gameBoardObj.flipCard); 
+            gameBoardObj.firstCard.removeEventListener('click', flipCard);
+            gameBoardObj.secondCard.removeEventListener('click', flipCard); 
             gameBoardObj.resetBoardVars();
         }
         else
         {
             gameBoardObj.lockBoard = true;
-            setTimeout(gameBoardObj.unflipCards, 1000, gameBoardObj.firstCard, gameBoardObj.secondCard);
+            setTimeout(unflipCards, 1000, gameBoardObj.firstCard, gameBoardObj.secondCard);
         }
     }
     else
@@ -43,31 +58,15 @@ gameBoard.prototype.flipCard = function()
 
 }
 
-gameBoard.prototype.unflipCards = function(firstCard, secondCard)
+unflipCards = function(firstCard, secondCard)
 {
-    gameBoardObj.firstCard.classList.remove('flip');
-    gameBoardObj.secondCard.classList.remove('flip');
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
     gameBoardObj.resetBoardVars();
 }
 
-gameBoard.prototype.checkMatch = function(firstCard, secondCard)
-{
-    if(gameBoardObj.firstCard.children[0].src === gameBoardObj.secondCard.children[0].src)
-        return true;
-    return false;
-}
-
-gameBoard.prototype.resetBoardVars = function()
-{
-    gameBoardObj.firstCard = null;
-    gameBoardObj.secondCard = null;
-    gameBoardObj.hasFlippedCard = false;
-    gameBoardObj.lockBoard = false;
-}
-
 const cards = document.getElementsByClassName('cellDiv');
-var gameBoardObj = new gameBoard(cards);
-for(var i = 0; i < gameBoardObj.cards.length; i++)
+for(var i = 0; i < cards.length; i++)
 {
-    gameBoardObj.cards[i].addEventListener('click', gameBoardObj.flipCard);
+    cards[i].addEventListener('click', flipCard);
 }
