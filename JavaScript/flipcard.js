ebf6b745fe01b725e974b29ae5741bf2a9e346e4
nahlaahmed97 <nahla.elaskry97@gmail.com>
2019-12-05 14:51:11 +0200

    added levels class

--- JavaScript/flipcard.js
+++ JavaScript/flipcard.js
@@ -17,3 +17,62 @@ function myFunction(x)
         document.getElementById("myDropdown").classList.add("show");
     }
 }
+
+
+var levels=document.getElementsByClassName('element_radio');
+var play=document.getElementsByClassName('play')[0];
+function Time (){
+    this.time=0;
+    this.level;
+
+}
+
+Time.prototype.getlevel=function(){
+    return this.level;
+}
+
+Time.prototype.setlevel=function(){
+    var elementChecked, selector ;
+    for(i = 0; i < levels.length; i++)
+    {
+        if(levels[i].checked)
+        {
+            selector = 'label[for=' + levels[i].id+ ']';
+            var label=document.querySelector(selector);
+            elementChecked=label.innerHTML;
+        }
+
+    }  
+    this.level=elementChecked;
+ }
+ Time.prototype.gettime=function(){
+    console.log(this.time);
+}
+
+
+Time.prototype.settime=function(){
+    if(this.level=="easy")
+    {
+        this.time=3000;
+    }
+
+    if(this.level=="intermediate")
+    {
+        this.time=2000;
+    }
+    if(this.level=="hard")
+    {
+        this.time=1000;
+    }
+}
+
+
+ var easy = new Time();
+  play.addEventListener("click",function()
+{
+    if(time!=0)
+    {easy.setlevel();
+    easy.getlevel();
+    easy.settime();
+    easy.gettime();}
+});
