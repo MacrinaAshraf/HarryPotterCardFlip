9401a59bbfda455d232e943766d13deb3eb29265
DaliaMatter <daliaamohamed243@gmail.com>
2019-12-06 11:19:12 +0200

    Add About Us Screen
    HTML By Nahla.
    CSS By Dalia
    
    Co-Authored-By: nahlaahmed97 <nahlaahmed97@users.noreply.github.com>

---
+++ JavaScript/aboutUs.js
@@ -0,0 +1,8 @@
+var backToMainBtn = document.getElementsByClassName('backToMain')[0];
+
+function returnToMenu()
+{
+    window.location.href="flipcard.html";
+}
+
+backToMainBtn.addEventListener('click', returnToMenu);
