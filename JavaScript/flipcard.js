c04d7d4279ab35537314073999914949bc1251dd
DaliaMatter <daliaamohamed243@gmail.com>
2019-12-06 12:18:16 +0200

    Add 3 Folders for each Characters
    Each Folder has the Characters' Pics which will fill the Board Game after choise the Character and start play

--- JavaScript/flipcard.js
+++ JavaScript/flipcard.js
@@ -1,4 +1,4 @@
-function myFunction(x)
+function MenuClick(x)
 {
     x.classList.toggle("change");
     if (document.getElementById("myDropdown").classList.contains('show'))
@@ -6,7 +6,6 @@ function myFunction(x)
         var dropdowns = document.getElementsByClassName("dropdown-content")[0];
             {
             var openDropdown = dropdowns;
-            // Close the dropdown if the user clicks outside of it
             if (openDropdown.classList.contains('show')) {
             openDropdown.classList.remove('show');
             }
