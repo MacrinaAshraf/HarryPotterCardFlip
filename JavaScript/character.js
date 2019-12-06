99766febae7d4ab45061fd577ecdb00b06c422df
DaliaMatter <daliaamohamed243@gmail.com>
2019-12-06 15:35:22 +0200

    Update Characters' Pics Array

--- JavaScript/character.js
+++ JavaScript/character.js
@@ -3,9 +3,9 @@ var character = function()
 {
     this.charName;
     //these arrays should store the sources of the characters' photos
-    this.harry = ['harry'];
-    this.hermonie = ['hermonie'];
-    this.ron = ['ron'];
+    this.harry = ['Harry1','Harry2','Harry3','Harry4','Harry5','Harry6','Harry7','Harry8'];
+    this.hermonie = ['Hermonie1','Hermonie2','Hermonie3','Hermonie4','Hermonie5','Hermonie6','Hermonie7','Hermonie8'];
+    this.ron = ['Ron1','Ron2','Ron3','Ron4','Ron5','Ron6','Ron7','Ron8'];
     //this array will be returned
     this.charArr;
 }
